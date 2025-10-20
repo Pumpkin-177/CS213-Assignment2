ff13e9acd287ef156f568df83d809f7c6c0a3505
haneenaymanmo <haninaymam@gmail.com>
2025-10-20 10:39:08 -0700

    First Version of Modularization Code

---
+++ Source/MainComponent.h
@@ -0,0 +1,20 @@
+#include <JuceHeader.h>
+#include "PlayerGUI.h"
+
+class MainComponent : public juce::AudioAppComponent,
+	public juce::Component
+{
+public:
+	MainComponent();
+	~MainComponent() override;
+
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+	void releaseResources() override;
+	void paint(juce::Graphics& g) override;
+	void resized() override;
+
+private:
+	PlayerGUI Player1;
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
---
+++ Source/PlayerAudio.cpp
@@ -0,0 +1,78 @@
+#include "PlayerAudio.h"
+
+PlayerAudio::PlayerAudio()
+{
+    formatManager.registerBasicFormats();
+}
+
+PlayerAudio::~PlayerAudio()
+{
+
+}
+
+void PlayerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void PlayerAudio::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+bool PlayerAudio::loadFile(const juce::File& file)
+{
+    if (file.existsAsFile())
+    {
+        if (auto* reader = formatManager.createReaderFor(file))
+        {
+            transportSource.stop();
+            transportSource.setSource(nullptr);
+            readerSource.reset();
+
+            readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+
+            transportSource.setSource(readerSource.get(),
+                0,
+                nullptr,
+                reader->sampleRate);
+            transportSource.start();
+        }
+    }
+    return true;
+}
+
+void PlayerAudio::start()
+{
+    transportSource.start();
+}
+
+void PlayerAudio::stop()
+{
+    transportSource.stop();
+}
+
+void PlayerAudio::setGain(float gain)
+{
+    transportSource.setGain(gain);
+}
+
+void PlayerAudio::setPosition(double pos)
+{
+    transportSource.setPosition(pos);
+}
+
+double PlayerAudio::getPosition() const
+{
+    return transportSource.getCurrentPosition();
+}
+
+double PlayerAudio::getLength() const
+{
+    return transportSource.getLengthInSeconds();
+}
