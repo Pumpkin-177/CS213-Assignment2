d34b632da65ec0ba49c33f400d32f5366515f4c8
haneenaymanmo <haninaymam@gmail.com>
2025-11-04 08:31:09 -0800

    Add second audio player and update audio handling

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,37 +2,44 @@
 
 MainComponent::MainComponent()
 {
-
-
     addAndMakeVisible(player1);
+	addAndMakeVisible(player2);
     setAudioChannels(0, 2);
     player1.loadLocation();
-	player1.setupAfterFileLoad(player1.getLastFile());
+	player2.loadLocation();
+    player1.setupAfterFileLoad(player1.getLastFile());
+	player2.setupAfterFileLoad(player2.getLastFile());
 }
 
 MainComponent::~MainComponent()
 {
-	player1.saveLocation();
+    player1.saveLocation();
+	player2.saveLocation();
     shutdownAudio();
 }
 
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     player1.prepareToPlay(samplesPerBlockExpected, sampleRate);
+    player2.prepareToPlay(samplesPerBlockExpected, sampleRate);
+
+    mixersource.addInputSource(player1.getAudioSource(), false);
+    mixersource.addInputSource(player2.getAudioSource(), false);
+    mixersource.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    player1.getNextAudioBlock(bufferToFill);
+    mixersource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
-    player1.releaseResources();
+    mixersource.releaseResources();
 }
-
 
 void MainComponent::resized()
 {
-    player1.setBounds(10, 10, getWidth() - 20, getHeight() - 20);
+    player1.setBounds(10, 10, (getWidth() / 2) - 20, getHeight() - 20);
+    player2.setBounds((getWidth() / 2) + 10, 10, (getWidth() / 2) - 20, getHeight() - 20);
 }
